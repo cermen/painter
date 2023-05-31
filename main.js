a5b5cc49ec8a9bc6dcf0a7d36489b312581d3b19
jyu <yjs01016@naver.com>
2023-05-31 22:23:13 +0900

    색상 바꾸기 기능 구현

--- main.js
+++ main.js
@@ -7,7 +7,6 @@ let activated = false;
 
 // 직선 그리기 기능
 // canvas.addEventListener("click", (event) => {
-//     console.log(event.offsetX, event.offsetY, activated);
 //     if (!activated) {
 //         ctx.moveTo(event.offsetX, event.offsetY);
 //         activated = true;
@@ -20,13 +19,13 @@ let activated = false;
 
 canvas.addEventListener("mousedown", (event) => {
     activated = true;
+    // setColor();
     ctx.beginPath();
     ctx.moveTo(event.offsetX, event.offsetY);
 })
 
 canvas.addEventListener("mousemove", (event) => {
     if (activated) {
-        console.log(event.offsetX, event.offsetY);
         ctx.lineTo(event.offsetX, event.offsetY);
         ctx.stroke();
     }
@@ -39,3 +38,45 @@ canvas.addEventListener("click", () => {
 canvas.addEventListener("mouseleave", () => {
     activated = false;
 });
+
+paletteColors.addEventListener("click", (event) => {
+    if (event.target.className === "palette__color") {
+        const rgbColor = event.target.style.backgroundColor;
+        ctx.strokeStyle = rgbColor;
+        const rgbs = rgbColor.substring(4, rgbColor.length - 1).split(',');
+        const [r, g, b] = rgbs.map(n => parseInt(n));
+        currentColor.value = rgbToHex(r, g, b);
+    }
+});
+
+currentColor.addEventListener("input", setColor);
+currentColor.addEventListener("change", setColor);
+
+function setColor() {
+    const {r, g, b} = hexToRgb(currentColor.value);
+    ctx.strokeStyle = `rgb(${r}, ${g}, ${b})`;
+}
+
+function rgbToHex(red, green, blue) {
+    const redHex = red.toString(16).padStart(2, '0');
+    const greenHex = green.toString(16).padStart(2, '0');
+    const blueHex = blue.toString(16).padStart(2, '0');
+  
+    const hexCode = '#' + redHex + greenHex + blueHex;
+  
+    return hexCode;
+}
+
+function hexToRgb(hex) {
+    const tempHex = hex.replace('#', '');
+  
+    const red = parseInt(tempHex.substring(0, 2), 16);
+    const green = parseInt(tempHex.substring(2, 4), 16);
+    const blue = parseInt(tempHex.substring(4, 6), 16);
+  
+    return {
+        r: red,
+        g: green,
+        b: blue
+    };
+}
